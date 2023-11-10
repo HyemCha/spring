8e98863a5f89b230e956526f689a7e4832ee5732
HyemCha <chm6194@gmail.com>
2023-11-10 18:59:37 +0900

    주문 서비스 테스트

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("memberA");
+
+        // when
+//        Long savedId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(savedId);
+
+        // then
+//        assertThat(findMember.getId()).isEqualTo(member.getId());
+//        assertThat(findMember.getName()).isEqualTo(member.getName());
+//        assertThat(findMember).isEqualTo(member);
+    }
+}
